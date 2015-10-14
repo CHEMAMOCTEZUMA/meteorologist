8738b1d89c992f394fa5b6b3b2451b547bc1faae
CHEMAMOCTEZUMA <jmoctezu@chicagobooth.edu>
2015-10-14 13:36:20 -0500

    Street Coords Done
    Was very confused at first on how to begin, but piazza was the perfect
    channel to get things up and running

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -16,12 +16,19 @@ class GeocodingController < ApplicationController
     # A URL-safe version of the street address, with spaces and other illegal
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
+  
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{url_safe_street_address}" 
+    # You should replace the Mountain View address with the street address the user typed
+ 
+    raw_data = open(url).read
 
+    require 'json'
 
+    parsed_data = JSON.parse(raw_data)
 
-    @latitude = "Replace this string with your answer."
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
 
-    @longitude = "Replace this string with your answer."
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
     render("street_to_coords.html.erb")
   end
